ac4f41b0d5b1f8dbe3888b7b731434eac23c984c
James <jaimersonaraujo@gmail.com>
2017-01-25 23:17:30 -0300

    Cache results because of github api limit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ import { numberOfRepositories } from 'api';
 export function App(){
   return {
     render: (container)=>{
-      let languages = ['ruby', 'javascript', 'java'];
+      let languages = ['ruby', 'javascript', 'java', 'scala', 'go', 'elixir',
+                       'haskell', 'c', 'cpp', 'lua', 'python'];
       languages.forEach((lang) =>{
         numberOfRepositories(lang).then((number) => {
           let div = document.createElement('div');
